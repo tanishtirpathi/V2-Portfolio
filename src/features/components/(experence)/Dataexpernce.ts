f4a375787574081517c31cfd48df1dbdbab00b39
tanish tirpathi <tanishtirpathi0@gmail.com>
2026-02-26 18:46:03 +0000

    production mess

--- src/features/components/(experence)/Dataexpernce.ts
+++ src/features/components/(experence)/Dataexpernce.ts
@@ -13,7 +13,7 @@ export const dataExperence: DataExperence[] = [  {
     image:"/images/Exp/SIH.jpg",
     title: "Smart India Hackathon – Internal Winner",
     company: "College Internal Round",
-    startDate: "September 2025",
+    startDate: "September-2025",
     description:
       "Collaborated in a competitive team environment to design and develop a problem-solving prototype under time constraints. Contributed to system architecture, core logic development, and final presentation delivery. Secured 1st position in internal selection round.",
     Technologies: ["LLM","Mongodb", "Express", "React" , "Next js" , "node-js "]
@@ -23,7 +23,7 @@ export const dataExperence: DataExperence[] = [  {
     image:"/images/Exp/IBM.jpg",
     title: "AI Chatbot Developer – Hackathon Winner",
     company: "IBM District Level Hackathon",
-    startDate: "December 2024",
+    startDate: "December-2024",
     description:
       "Built a functional AI chatbot designed to solve real-world queries using NLP-based response handling. Designed conversation flow, implemented backend logic, and deployed a working prototype within hackathon constraints. Secured 1st position at district level.",
     Technologies: [ "Python", "NLP", "API Integration"]
@@ -34,7 +34,7 @@ export const dataExperence: DataExperence[] = [  {
       image:"/images/Exp/Scaler.png",
     title: "Remote Trainee / Intern",
     company: "Scaler",
-    startDate: "june 2024",
+    startDate: "June-2024",
     description:
       "Completed structured training in basic programming, LinkedIn optimization, and introductory Agentic AI concepts. Built small practice applications and explored AI workflow fundamentals.",
     Technologies: ["JavaScript", "Programming Fundamentals", "Agentic AI Basics"]
