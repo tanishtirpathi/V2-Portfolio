010a6b7ed579d7a81a26027f8a35336886c92f25
tanish tirpathi <tanishtirpathi0@gmail.com>
2026-03-03 14:29:46 +0000

    added: css

--- src/app/(main)/projects/[slug]/page.tsx
+++ src/app/(main)/projects/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { MDXRemote } from "next-mdx-remote/rsc";
-
+import "../../../mdx.css";
 export default async function ProjectPage(props: { params: { slug: string } }) {
   const params = await props.params;
   const slug = params?.slug;
@@ -19,5 +19,7 @@ export default async function ProjectPage(props: { params: { slug: string } }) {
   console.log("File path:", filePath);
   const source = fs.readFileSync(filePath, "utf8");
 
-  return <MDXRemote source={source} />;
+  return (<div className="mdx-wrapper ">
+    <MDXRemote source={source} />
+  </div>);
 }
