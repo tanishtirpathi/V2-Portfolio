0388d29c65ba546a5faf2dc27500af10208c6d81
Tanish Tirpathi <tanishtirpathi0@gmail.com>
2026-07-20 02:49:47 +0530

    change reusme

--- src/app/(main)/Resume/page.tsx
+++ src/app/(main)/Resume/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <div className="relative w-full h-[700px] rounded-lg overflow-hidden border">
           <div className="mx-auto max-w-2xl">
             <iframe
-              src="https://drive.google.com/file/d/1B5LVgzsEgUfE4WsZuihHuPNB2Zqqwggg/preview"
+              src="https://drive.google.com/file/d/1ItQfblCng60un3uYfpz0aoDk8V4naVQR/preview"
               className="min-h-screen w-full"
             ></iframe>
           </div>
@@ -28,7 +28,7 @@ export default function Home() {
 
         <div className="flex justify-center mt-6">
           <a
-            href="https://drive.google.com/uc?export=download&id=1V5Sx4a7f4NAJRqR3DA7FpDdbpiJPBHyz"
+            href="https://drive.google.com/uc?export=download&id=1ItQfblCng60un3uYfpz0aoDk8V4naVQR"
             className="px-6 py-3 rounded-lg border border-black hover:bg-black hover:text-white transition"
           >
             Download Resume
